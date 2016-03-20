ebca8e1db67f92c63f60134f5235ea114e96cf50
miyamoriaoi <miyamori@126.com>
2016-03-20 12:58:39 +0800

    U

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,7 @@
 class Video < ActiveRecord::Base
   extend Enumerize
   enumerize :permission, in: {all: 0, user: 1}
+  # 这里可以改成从video_type 获取
   enumerize :type, in:{animation: 0, music: 1, game: 2, science:3, otomad: 4, entertainment:5  }
   has_many :video_comments
   belongs_to :user
--- app/models/video_type.rb
+++ app/models/video_type.rb
@@ -1,4 +1,3 @@
 class VideoType < ActiveRecord::Base
-  extend Enumerize
-  enumerize :name, in:{animation: 0, music: 1, game: 2, science:3, otomad: 4, entertainment:5  }
+
 end
